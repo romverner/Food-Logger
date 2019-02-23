e2a0f7fe0988b2728932962e258e4e1197b6674c
Roman Verner <romverj@gmail.com>
2019-02-23 10:46:37 -0500

    Basic seeds for db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger) VALUES ('cheeseburger');
+INSERT INTO burgers (burger) VALUES ('double quarter pounder');
+INSERT INTO burgers (burger) VALUES ('big mac');
+INSERT INTO burgers (burger) VALUES ('gothic');
+INSERT INTO burgers (burger) VALUES ('portobello');

081e3c6c8f2f29c2bb78a873a9424a9153cdf78d
Roman Verner <romverj@gmail.com>
2019-02-24 15:21:30 -0500

    Route skeleton added

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router  = express.Router();
+var burger  = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/api/burgers", (req, res) => {
+
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+
+});
+
+module.exports = router;

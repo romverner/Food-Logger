5b39b522977fe8cd9c2ecb9f66f9ec348305caa5
Roman Verner <romverj@gmail.com>
2019-02-26 18:31:43 -0500

    Typo fix

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -27,7 +27,7 @@ router.put("/api/burgers/:id", (req, res) => {
 
     burger.updateOne({
         devoured: req.body.devoured
-    }, dev, (result) => {
+    }, devoured, (result) => {
         if (result.changedRows == 0) {
             return res.status(404).end();
         } else {
